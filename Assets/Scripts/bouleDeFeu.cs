b34c9289e48ff2fecf1e32a83d7847e36cc7b9b1
xofni <fakenewsProject@hotmail.com>
2018-10-28 23:40:54 +0100

    Work on the Dragon

--- Assets/Scripts/bouleDeFeu.cs
+++ Assets/Scripts/bouleDeFeu.cs
@@ -7,6 +7,7 @@ public class bouleDeFeu : MonoBehaviour {
     public Transform fire;
     public Transform dragon;
     public Transform burst;
+    bool flagFire = true;
 
     // Use this for initialization
     void Start () {
@@ -15,22 +16,53 @@ public class bouleDeFeu : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if ((dragon.position.y > 3 && dragon.position.y < 3.009) || (dragon.position.y > 4 && dragon.position.y < 4.009))
+        if ((dragon.position.y > 3 && dragon.position.y < 3.009) && flagFire == true)
         {
-            print("Fire");
-            Transform newFire = Instantiate(fire);
-            newFire.position = dragon.position;
-            newFire.GetComponent<Rigidbody>().AddForce(1500, 20, 0);
-    
+            flagFire = false;
+            shootFire();
+        }
+        if ((dragon.position.y > 4 && dragon.position.y < 4.009) && flagFire == true)
+        {
+            flagFire = false;
+            shootFire();
         }
 
+    }
+    void shootFire()
+    {
+        
+        print("Fire");
+        Transform newFire = Instantiate(fire);
+        newFire.position = dragon.position;
+        if (newFire.position.z > 2)
+        {
+            newFire.GetComponent<Rigidbody>().AddForce(500, 20, -100);
+        } else if (newFire.position.z < -2)
+        {
+            newFire.GetComponent<Rigidbody>().AddForce(500, 20, 100);
+        } else
+        {
+            newFire.GetComponent<Rigidbody>().AddForce(500, 20, 0);
+        }
+
+            StartCoroutine(Example());
+        
+    }
+    IEnumerator Example()
+    {
+        print("flagFire : " + flagFire);
+        print("T1----------------------------------------------------------- " + Time.time);
+        yield return new WaitForSeconds(100);
+        print("T2----------------------------------------------------------- " + Time.time);
+        flagFire = true;
+        print("flagFire : " + flagFire);
     }
     void OnCollisionEnter(Collision maCollision)
     {
 
         if (maCollision.gameObject.name == "castel")
         {
-            print("Burst");
+            print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ Burst");
         }
 
     }
--- Assets/Scripts/dragon.cs
+++ Assets/Scripts/dragon.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class dragon : MonoBehaviour {
 
-    public float boundHeightMin = 2.0f;
-    public float boundHeightMax = 13.0f;
+    public float boundHeightMin = 3.0f;
+    public float boundHeightMax = 7.0f;
     public float boundRightMin = -2.0f;
     public float boundRightMax = 7.0f;
     public float boundLeftMin = -5.0f;
@@ -23,30 +23,30 @@ public class dragon : MonoBehaviour {
 	void Update () {
         if (Input.GetKey(KeyCode.S))
         {
-            gameObject.GetComponent<Rigidbody>().AddForce(10, 50, -2);
+            gameObject.GetComponent<Rigidbody>().AddForce(5, 10, -20);
             flagStop = false;
             countBound = 0;
             countBoundinit = 0;
-            print("S");
+            print("S 5, 20, -20");
         }
         if (countBoundinit >= 10 && countBoundinit < 100 && flagStop == false)
         {
-            gameObject.GetComponent<Rigidbody>().AddForce(2, 30, -20);
-            print("countBoundinit");
+            gameObject.GetComponent<Rigidbody>().AddForce(2, 15, 20);
+            print("countBoundinit 2,15,20");
             countBoundinit = 100;
 
         }
         if (countBound % 200 == 0 && flagStop == false)
         {
-            gameObject.GetComponent<Rigidbody>().AddForce(5, 30, 3);
+            gameObject.GetComponent<Rigidbody>().AddForce(5, 15, -20);
             countBound++;
-            print("Bong");
+            print("Bong 5, 15 -20");
         }
         if (countBound % 45 == 0 && flagStop == false)
         {
-            gameObject.GetComponent<Rigidbody>().AddForce(-2, 10, -5);
+            gameObject.GetComponent<Rigidbody>().AddForce(-2, 10, 20);
             countBound++;
-            print("Bing");
+            print("Bing -2,10,20");
         }
         if (countBound > 1000)
         {
@@ -60,18 +60,20 @@ public class dragon : MonoBehaviour {
         
         if (transform.position.y <= boundHeightMin && flagStop == false)
         {
-            gameObject.GetComponent<Rigidbody>().AddForce(0, 50, 0);
+            gameObject.GetComponent<Rigidbody>().AddForce(0, 20, 0);
             countBound++;
             countBoundinit++;
+            print("MinHeight + 20");
         }
         
         if (transform.position.y > boundHeightMax && flagStop == false)
         {
-            gameObject.GetComponent<Rigidbody>().AddForce(0, -20, 3);
+            gameObject.GetComponent<Rigidbody>().AddForce(0, -10, 3);
+            print("MaxHeight + -20, 3");
         }
         if (transform.position.x <= boundRightMin && flagStop == false)
         {
-            gameObject.GetComponent<Rigidbody>().AddForce(3, 0, 0);
+            gameObject.GetComponent<Rigidbody>().AddForce(3, 1, 0);
             countBound++;
         }
         if (transform.position.x > boundRightMax && flagStop == false)
@@ -81,13 +83,15 @@ public class dragon : MonoBehaviour {
         }
         if (transform.position.z <= boundLeftMin && flagStop == false)
         {
-            gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
-            gameObject.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
-            gameObject.GetComponent<Rigidbody>().AddForce(0, 5, 10);
+            // gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            // gameObject.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+            gameObject.GetComponent<Rigidbody>().AddForce(0, 5, 20);
+            print("LeftMin + (5, 10");
         }
         if (transform.position.z > boundLeftMax && flagStop == false)
         {
-            gameObject.GetComponent<Rigidbody>().AddForce(0, 5, -10);
+            gameObject.GetComponent<Rigidbody>().AddForce(0, 5, -20);
+            print("LeftMax + 5, -10");
         }
         
 
